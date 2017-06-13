8a52562e3780294b7a7710be9bc9d818e1ed8da9
leo <gpigliapoco@gmail.com>
2017-06-12 21:00:33 -0300

    se agrego archivo de conexion

---
+++ config/global.php
@@ -0,0 +1,10 @@
+<?php 
+
+define("DB_HOST","localhost");
+define("DB_Name","tahin");
+define("DB_username","root");
+define("DB_PASSWORD","1234");
+define("DB_ENCODE","utf8");
+define("DB_NOMBRE","TAHIN_sistema");
+
+ ?>
