35f6007f3b1e6e51d0015bd31fdc2d74e02cef58
leo <gpigliapoco@gmail.com>
2017-06-13 00:30:31 -0300

    formulario de operadores agregado todos los campos

--- vistas/categoria.php
+++ vistas/categoria.php
@@ -57,7 +57,28 @@ require 'header.php';
                           <div class="form-group"> <!-- City-->
                               <label for="city_id" class="control-label">Ciudad</label>
                               <input type="text" class="form-control" id="city_id" name="city" placeholder="Ciudad">
-                          </div>                               
+                          </div>   
+                          <div class="form-group"> <!-- State Button -->
+                              <label for="state_id" class="control-label">Provincia</label>
+                              <select class="form-control" id="state_id">
+                                  <option value="Buenos aires">Buenos aires</option>
+                                  <option value="Buenos aires">Santa fe</option>
+                                  <option value="Cordoba">Cordoba</option>
+                                  <option value="Entre rios">Entre rios</option>
+                                  <option value="Misiones">Misiones</option>
+                                  <option value="Corrientes">Corrientes</option>
+                                  <option value="Tucuman">Tucuman</option> 
+                              </select>                    
+                          </div> 
+                          <div class="form-group"> <!-- City-->
+                              <label for="city_id" class="control-label">Telefono</label>
+                              <input type="text" class="form-control" id="city_id" name="city" placeholder="Telefono">
+                          </div>     
+                          <div class="form-group"> <!-- City-->
+                              <label for="city_id" class="control-label">Observacion</label>
+                               <textarea class="form-control" id="message" name="message" placeholder="Observacion" rows="7"></textarea>
+                          </div>     
+
                           <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i> Guardar</button>
 
